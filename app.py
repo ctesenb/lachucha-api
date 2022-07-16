98c004b62252b473d510a8cc386480242494c095
ctesenb <ctesenb@gmail.com>
2022-07-16 06:14:03 -0500

    asd

--- app.py
+++ app.py
@@ -2,9 +2,25 @@ from fastapi import FastAPI
 from routes.pedido import pedido
 from routes.detallepedido import detallepedido
 from routes.plato import plato
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
 app.include_router(pedido)
 app.include_router(detallepedido)
 app.include_router(plato)
+
+origins = [
+    "https://lachucha-api.herokuapp.com/",
+    "http://localhost:3000/",
+    "http://localhost:8000/",
+    "http://localhost:5000/",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
