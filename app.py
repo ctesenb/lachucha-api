bbe36a2643ce9e88f582654f606651aadc8838f2
ctesenb <ctesenb@gmail.com>
2022-07-16 06:15:45 -0500

    asdasd

--- app.py
+++ app.py
@@ -15,6 +15,13 @@ origins = [
     "http://localhost:3000/",
     "http://localhost:8000/",
     "http://localhost:5000/",
+    "http://localhost:4200/",
+    "http://localhost:8080/",
+    "http://localhost:8081/",
+    "http://localhost:8082/",
+    "http://localhost:8083/",
+    "http://localhost:8084/",
+    "http://localhost:8085/",
 ]
 
 app.add_middleware(
