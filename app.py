19da03ce7d3aebbb283400b7d5069c3ee86450bb
ctesenb <ctesenb@gmail.com>
2022-07-16 06:19:08 -0500

    ApiCorregido

--- app.py
+++ app.py
@@ -5,10 +5,17 @@ from routes.plato import plato
 from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+origins = ["*"]
+
 app.add_middleware(
-        CORSMiddleware,
-        allow_origins=['*']
-    )
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(pedido)
 app.include_router(detallepedido)
 app.include_router(plato)
